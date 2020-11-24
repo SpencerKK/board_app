61bcb1397906f9ddc9722d9b3281783c3bd4bf40
SpencerKK <kenealys@me.com>
2020-11-24 06:46:24 -0800

    Landing.js positioning setup at good starting point
    adjusting screensize pushes images now

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Draggable from 'react-draggable';
+import Draggable from "react-draggable";
 
 // images
 import canserefoglu from "../assets/images/can-serefoglu.jpg";
@@ -8,28 +8,35 @@ import raphaelnast from "../assets/images/raphael-nast.jpg";
 import shinjirice from "../assets/images/shingi-rice.jpg";
 
 const Landing = () => {
-    return (
-        <div className="landing">
+   return (
+      <div className="landing">
+         <div className="landing-images">
             <div className="landing-content">
-                <h1>moodboardr</h1>
-                <p>Conceptualize your next big project. Collab with creatives. Be inspired.</p>
+               <h1>moodboardr</h1>
+               <p>
+                  Conceptualize your next big project. Collab with creatives. Be
+                  inspired.
+               </p>
             </div>
-            <div className="landing-images">
-                <Draggable>
-                    <img src={canserefoglu} draggable="false" alt="can-serfoglu" />
-                </Draggable>
-                <Draggable>
-                    <img src={eliapellegrini} draggable="false" alt="elia-pellegrini" />
-                </Draggable>
-                <Draggable>
-                    <img src={raphaelnast} draggable="false" alt="raphael-nast" />
-                </Draggable>
-                <Draggable>
-                    <img src={shinjirice} draggable="false" alt="shingi-rice" />
-                </Draggable>
-            </div>
-        </div>
-    )
-}
+            <Draggable bounds="parent">
+               <img src={canserefoglu} draggable="false" alt="can-serfoglu" />
+            </Draggable>
+            <Draggable bounds="parent">
+               <img
+                  src={eliapellegrini}
+                  draggable="false"
+                  alt="elia-pellegrini"
+               />
+            </Draggable>
+            <Draggable bounds="parent">
+               <img src={raphaelnast} draggable="false" alt="raphael-nast" />
+            </Draggable>
+            <Draggable bounds="parent">
+               <img src={shinjirice} draggable="false" alt="shingi-rice" />
+            </Draggable>
+         </div>
+      </div>
+   );
+};
 
 export default Landing;
